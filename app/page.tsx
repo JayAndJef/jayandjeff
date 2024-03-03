0be1b67bed175b50dbdb7c0ad6e898ce907e4905
JayAndJef <109392310+JayAndJef@users.noreply.github.com>
2024-03-02 16:46:00 -0800

    add projects popup

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,8 @@
+import { BackgroundGradient } from "@/components/ui/background-gradient";
 import { WavyBackground } from "@/components/ui/wavy-background";
 
 export default function Home() {
-    const words = [
-        {
-            text: "Build",
-        },
-        {
-            text: "awesome",
-        },
-        {
-            text: "apps",
-        },
-        {
-            text: "with",
-        },
-        {
-            text: "Aceternity.",
-            className: "text-blue-500 dark:text-blue-500",
-        },
-    ];
+
 
     return (
         <main>
@@ -28,7 +12,7 @@ export default function Home() {
                         <h2 className="text-2xl md:text-3xl xl:text-5xl font-semibold bg-gradient-to-b from-slate-300 to-slate-400 py-4 bg-clip-text text-transparent">
                             Hi, I&rsquo;m
                         </h2>
-                        <h1 className="text-[4rem] md:text-[5rem] lg:text-[7rem] xl:text-[8rem] 2xl:text-[9rem] min-[1920px]:text-[10rem] font-bold bg-gradient-to-r from-purple-500 to-purple-600 bg-clip-text text-transparent">
+                        <h1 className="text-[4rem] md:text-[5rem] lg:text-[7rem] xl:text-[8rem] 2xl:text-[9rem] min-[1920px]:text-[10rem] font-bold bg-gradient-to-r from-purple-500/75 to-purple-600/75 bg-clip-text text-transparent">
                             Jayden Q
                         </h1>
                         <h2 className="text-xl md:text-2xl xl:text-4xl font-semibold bg-gradient-to-b from-slate-300 to-slate-400 py-4 bg-clip-text text-transparent">
@@ -37,9 +21,6 @@ export default function Home() {
                     </div>
                     <div className="flex w-full 2xl:w-1/4 min-[1920px]:w-1/3 border-l-2 border-purple-800">
                         <ul className="list-none px-10 text-2xl md:text-3xl lg:text-4xl text-slate-300 space-y-5">
-                            <li>
-                                <a href="#about-me">about me</a>
-                            </li>
                             <li>
                                 <a href="#projects">projects</a>
                             </li>
@@ -51,8 +32,10 @@ export default function Home() {
                 </div>
             </WavyBackground>
 
-            <div className="w-screen h-screen z-0 mt-[100vh] bg-slate-300">
-                something else here
+            <div className="w-full h-screen rounded-md z-0 mt-[100vh] bg-slate-800/75 backdrop-blur-md p-5 flex flex-col">
+                <h1 className="text-[3rem] md:text-[4rem] lg:text-[5rem] font-semibold text-slate-300 self-center justify-center">
+                    Projects
+                </h1>
             </div>
         </main>
     );
